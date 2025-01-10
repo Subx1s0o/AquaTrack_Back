a92fce8f0ca3708c8e3ee8b8643d9b10aef497ab
Subx1s0o <nazarsabardin@gmail.com>
2025-01-10 15:40:11 +0200

    feat: add auth checker and user checker

---
+++ libs/utils/userChecker.ts
@@ -0,0 +1,18 @@
+import { UserRepository } from '@/libs/db/user.repository';
+import { Action, UnauthorizedError } from 'routing-controllers';
+import Container from 'typedi';
+import { IUser } from '../db';
+
+export async function userChecker(action: Action): Promise<IUser> {
+  const userId = action.request.userId;
+  if (!userId) {
+    throw new Error('You are not logged in to do this');
+  }
+
+  const userRepository = Container.get(UserRepository);
+  const user = await userRepository.findOne({ _id: userId });
+  if (!user) {
+    throw new UnauthorizedError('You are not logged in to do this');
+  }
+  return user;
+}
