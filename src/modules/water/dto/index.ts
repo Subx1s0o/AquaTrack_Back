143b21ca77775038aeba5a75bb18d031f021ad08
Subx1s0o <nazarsabardin@gmail.com>
2025-01-14 23:07:56 +0200

    fix: remove unused DeleteWaterDTO export

--- src/modules/water/dto/index.ts
+++ src/modules/water/dto/index.ts
@@ -1,5 +1,4 @@
 export { AddWaterDTO } from './addWater';
 export { EditWaterDTO } from './editWater';
-export { DeleteWaterDTO } from './deleteWater';
 export { GetDailyWaterDTO } from './getDailyWater';
 export { GetMonthlyWaterDTO } from './getMonthlyWater';
